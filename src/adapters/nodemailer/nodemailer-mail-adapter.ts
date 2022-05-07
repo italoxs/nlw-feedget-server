50bc75f092f23c361cb95404b4409b5ea12658b7
Italo Sales <italo.x.sales@hotmail.com>
2022-05-07 10:20:27 -0300

    nlw feedget server finishied

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "196450b4de5035",
+    pass: "c2d794fa313517"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({subject, body}: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Italo Sales <italo.x.sales@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
