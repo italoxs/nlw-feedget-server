2f92d9ab37a09b15963ac2797770c7eb60487d81
Italo Sales <italo.x.sales@hotmail.com>
2022-05-07 21:14:47 -0300

    add try catch in routes

--- src/routes.ts
+++ src/routes.ts
@@ -8,19 +8,24 @@ export const routes = express.Router();
 routes.post('/feedbacks', async (request, response) => {
   const { type, comment, screenshot } = request.body;
 
-  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-  const nodemailerMailAdapter = new NodemailerMailAdapter();
+  try {
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+    const nodemailerMailAdapter = new NodemailerMailAdapter();
 
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbacksRepository,
-    nodemailerMailAdapter
-  );
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+      prismaFeedbacksRepository,
+      nodemailerMailAdapter
+    );
 
-  await submitFeedbackUseCase.execute({
-    type,
-    comment,
-    screenshot,
-  });
 
-  return  response.status(201).send();
+    await submitFeedbackUseCase.execute({
+      type,
+      comment,
+      screenshot,
+    });
+
+    return response.status(201).send();
+  } catch (err) {
+    return response.status(500).send();
+  }
 });
